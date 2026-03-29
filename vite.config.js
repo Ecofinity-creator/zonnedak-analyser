2fe41742a524026379cdbb37f99b21ca062a82c5
Ecofinity-creator <steven@ecofinity.eu>
2026-03-29 09:34:26 +0200

    Rename base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Pas 'zonnedak-analyzer' aan naar de naam van jouw GitHub repository
+// Pas 'zonnedak-analyser' aan naar de naam van jouw GitHub repository
 export default defineConfig({
   plugins: [react()],
-  base: '/zonnedak-analyzer/',
+  base: '/zonnedak-analyser/',
 })
